61b746f24488d41aee4e005c66f04ebf24ddfacb
Freewind <nowindlee@gmail.com>
2024-02-23 19:46:58 +0800

    best solution

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 TypeScript React AntDesign Popover Modify Style Demo
 =================================
 
-怎么给Popover自定义样式，比如背景色
+使用ConfigProvider给Popover自定义样式，比如背景色。
+
+每个组件的下面都有说明该组件用了哪些tokens以及默认值，非常方便。
+
+感谢赛奇
 
 ```
 npm install
--- src/hello.tsx
+++ src/hello.tsx
@@ -1,13 +1,16 @@
+import { Button, ConfigProvider, Popover } from 'antd';
 import React from 'react';
-import { Button, Popover } from 'antd'
-import { css } from '@emotion/css';
-
-const popoverStyle = css`
- background-color: red;
-`;
 
 export default function Hello() {
-  return <Popover open overlayInnerStyle={{ backgroundColor: 'red' }} trigger={'click'} title={'Hello'}>
-    <Button>Click</Button>
-  </Popover>
+  return <ConfigProvider theme={{
+    components: {
+      Popover: {
+        colorBgElevated: 'red'
+      }
+    }
+  }}>
+    <Popover trigger={'click'} title={'Hello'}>
+      <Button>Click</Button>
+    </Popover>
+  </ConfigProvider>
 };
